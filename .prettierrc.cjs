b42a151c5190e7a47060c8cd69341823ff2eaef9
Guy K <47288.koranat@gmail.com>
2023-08-13 23:01:57 +0700

    chore: trying to fix eslint

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-  trailingComma: 'es5',
+  printWidth: 80,
   tabWidth: 2,
+  useTabs: false,
   semi: true,
   singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf',
 };
