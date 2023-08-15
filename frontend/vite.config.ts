022edbd469b8817f4b6a28f0e1623f363e726fa1
koranat <47288.koranat@gmail.com>
2023-08-15 17:27:49 +0700

    chore: fix eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/code-of-conduct-29/"
+  base: '/code-of-conduct-29/',
 });
